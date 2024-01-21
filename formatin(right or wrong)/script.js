9970848bbc543bc30ccbfa84bc08c3e16676e6ee
amirhossein <147509867+amirhussein27@users.noreply.github.com>
2024-01-21 09:41:38 +0330

    Add files via upload

---
+++ formatin(right or wrong)/script.js
@@ -0,0 +1,34 @@
+let mybtn=document.getElementById("mybtn")
+mybtn.addEventListener('click',function(){
+  let gk=document.getElementById("gk")
+  let rb=document.getElementById("rb")
+  let cb1=document.getElementById("cb1")
+  let cb2=document.getElementById("cb2")
+  let lb=document.getElementById("lb")
+  let cm1=document.getElementById("cm1")
+  let cm2=document.getElementById("cm2")
+  let cm3=document.getElementById("cm3")
+  let cm4=document.getElementById("cm4")
+  let cf1=document.getElementById("cf1")
+  let cf2=document.getElementById("cf2")
+
+
+  if(gk.value =="flekken" 
+  && rb.value =="ben-mee" 
+  && cb1.value =="pinnock" 
+  && cb2.value =="goode"
+  && lb.value =="reguilon"
+  && cm1.value =="ghodos"
+  && cm2.value =="lewis-potter"
+  && cm3.value =="onyeka"
+  && cm4.value =="dasilva"
+  && cf1.value =="tony"
+  && cf2.value =="maupay"
+
+   ) {
+  alert("truuuuuuuuuu")
+  }
+  else{
+    alert("formation is not correct")
+  }
+})
